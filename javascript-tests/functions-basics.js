c0014da3bb912c3cde1ac34a8d4f3d6a37e41b2c
betsy-bawden <43550573+betsy-bawden@users.noreply.github.com>
2018-11-23 13:39:50 -0800

    Create functions-basics.js

---
+++ javascript-tests/functions-basics.js
@@ -0,0 +1,22 @@
+function simpleFunction () {
+    console.log("functions should consist of statements designed to perform a single task.");
+}
+
+function functionParameters () {
+
+}
+
+function functionReturn () {
+    console.log("Many functions return values.");
+}
+
+simpleFunction();
+
+functionParameters("Many functions take parameters");
+
+functionReturn();
+
+window.onload = {
+    const simpleFunction = document.getElementById('simple-function');
+    const functionCalls = document.getElementById('function-calls');   
+}
