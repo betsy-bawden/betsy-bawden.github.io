adf6e369651910825e99770555ea72080eeaf840
betsy-bawden <43550573+betsy-bawden@users.noreply.github.com>
2018-12-05 13:56:01 -0800

    Update radio-tests.js

--- javascript-tests/radio-tests.js
+++ javascript-tests/radio-tests.js
@@ -1,17 +1,28 @@
 
-const myForm = document.getElementById('sort-type');
+window.onload = () => {
 
 myForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const value = document.querySelector('input[name="sorter"]:checked').value;
-    if (value.toLowerCase() === 'green') {
-        setColor("blueBackground");
-        showChoice(value);
-    } else {
-        setColor("greenBackground");
-        showChoice(value);
-    }
-});
+    const color = value.toLowerCase();
+if (color === 'blue') {
+    setColor("blueBackground");
+    showChoice(value);
+} else if (color === 'green') {
+    setColor("greenBackground");
+    showChoice(value);
+} else if (color === 'red') {
+    setColor("redBackground");
+    showChoice(value);
+} else if (color === 'purple') {
+    setColor("purpleBackground");
+    showChoice(value);
+} else if (color === 'yellow') {
+    setColor("yellowBackground");
+    showChoice(value);
+};
+
+const myForm = document.getElementById('sort-type');
 
 let currentClass = null;
 
@@ -24,21 +35,3 @@ const setColor = (className) => {
     currentClass = className;
     element.classList.add(className);
 };
-
-if (value.toLowerCase() === 'blue') {
-    setColor("blueBackground");
-    showChoice(value);
-} else (value.toLowerCase() === 'green') {
-    setColor("greenBackground");
-    showChoice(value);
-} else (value.toLowerCase() === 'red') {
-    setColor("redBackground");
-    showChoice(value);
-} else (value.toLowerCase() === 'purple') {
-    setColor("purpleBackground");
-    showChoice(value);
-} else (value.toLowerCase() === 'yellow') {
-    setColor("yellowBackground");
-    showChoice(value);
-};
-
